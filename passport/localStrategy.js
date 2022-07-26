0270cf115578ad24cb420ab0d1307855928fd5d0
taeju-moon <pp2lycee@naver.com>
2022-07-26 22:44:14 +0900

    set eslint

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
-const User = require('../models/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
 
 module.exports = () => {
   passport.use(
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const http = require('http');
+
 const app = express();
 const { Server } = require('socket.io');
 const dotenv = require('dotenv');
 const passport = require('passport');
+
 const port = process.env.PORT || 3000;
 
 //써드파티 미들웨어 설정
@@ -21,9 +23,11 @@ app.listen(port, () => {
 });
 
 //라우팅
-const authRouter = required('./routes/auth');
-const adminRouter = required('./routes/admin');
-const clientRouter = required('./routes/client');
+const authRouter = require('./routes/auth');
+
+const adminRouter = require('./routes/admin');
+
+const clientRouter = require('./routes/client');
 
 app.use('/auth', authRouter);
 app.use('/admin', adminRouter);
@@ -36,4 +40,5 @@ const wsServer = new Server(httpServer);
 //브라우저가 1개만 있으면 되므로 모든 sockets으로 정보를 뿌릴 필요는 없음.
 wsServer.on('connection', (socket) => {
   console.log(wsServer.sockets.length);
+  console.log(socket);
 });
