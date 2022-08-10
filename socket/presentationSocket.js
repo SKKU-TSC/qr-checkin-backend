236fc4be02d1a53e57ad8deab356344783bfea52
Daniel2231 <daniel2231.dev@gmail.com>
2022-08-11 03:39:28 +0900

    production build, for rehersal

--- socket/presentationSocket.js
+++ socket/presentationSocket.js
@@ -1,11 +1,29 @@
+const textToSpeech = require('@google-cloud/text-to-speech');
 const User = require('../models/user');
+
+const client = new textToSpeech.TextToSpeechClient();
 
 function presentationSocket(socketIo) {
   socketIo.on('connection', (socket) => {
     socket.on('displaySet', async (studentId) => {
       const user = await User.findOne({ where: { studentId: studentId } });
       if (user) {
-        socketIo.sockets.emit('display', user);
+        const ttsRequest = {
+          input: { text: `${user.name}, ${user.major}` },
+          voice: {
+            languageCode: 'ko-KR',
+          },
+          audioConfig: {
+            audioEncoding: 'LINEAR16',
+            speakingRate: 1,
+          },
+        };
+        const [response] = await client.synthesizeSpeech(ttsRequest);
+        const socketRequest = {
+          data: user,
+          voiceTTS: response,
+        };
+        socketIo.sockets.emit('display', socketRequest);
       }
     });
   });
